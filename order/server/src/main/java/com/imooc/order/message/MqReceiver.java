37e9017fa419c7ed8221cb972a9d20fbb89f9eae
machi <machi@jcfc.cn>
2019-04-30 18:56:15 +0800

    rabbitmq todo

---
+++ order/server/src/main/java/com/imooc/order/message/MqReceiver.java
@@ -0,0 +1,14 @@
+package com.imooc.order.message;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class MqReceiver {
+    @RabbitListener(queues = "myQueue")
+    public void proecess(String message){
+        log.info("MqReceiver:{}",message);
+    }
+}
